df148e2054d9b727b8b183109c9bbb38885186d6
niloy <niloy64529@gmail.com>
2026-04-24 16:37:30 +0600

    Refactor Catalog model to team-centric structure

--- models/models.go
+++ models/models.go
@@ -4,11 +4,7 @@ type Catalog struct {
 	Version     int               `json:"version"`
 	LastUpdated string            `json:"last_updated"`
 	Leagues     map[string]League `json:"leagues"`
-	Seasons     map[string]Season `json:"seasons"`
-}
-
-type Season struct {
-	Teams map[string]TeamKits `json:"teams"`
+	Teams       map[string]Team   `json:"teams"`
 }
 
 type League struct {
@@ -18,20 +14,12 @@ type League struct {
 	IsPopular bool   `json:"is_popular,omitempty"`
 }
 
-type TeamKits struct {
-	Name      string            `json:"name"`
-	Logo      string            `json:"logo,omitempty"`
-	League    string            `json:"league"`
-	IsPopular bool              `json:"is_popular,omitempty"`
-	Kits      map[string]string `json:"kits"`
-}
-
 type Team struct {
 	Name      string                       `json:"name"`
 	Logo      string                       `json:"logo,omitempty"`
 	League    string                       `json:"league"`
 	IsPopular bool                         `json:"is_popular,omitempty"`
-	Seasons   map[string]map[string]string `json:"seasons"`
+	Seasons   map[string]map[string]string `json:"seasons"` // key: "2024", "2025", "2026"
 }
 
 type KitRecord struct {
